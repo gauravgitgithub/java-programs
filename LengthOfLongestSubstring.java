fd8e7cece8a6f6a37958ff1953a11d545c786ffb
Gaurav Raj Sinha <gauravraj.sinha@singleinterface.com>
2020-07-16 23:39:36 +0530

    Java First Program to get length of longest substring without repeating characters

---
+++ LengthOfLongestSubstring.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class LengthOfLongestSubstring{
+
+	static int getLengthOfLongestSubstring(String input){
+        int n = input.length(), toReturn = 0;
+        int[] index = new int[256]; //assuming int array as direct access table (extended ASCII)
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[input.charAt(j)], i);
+            toReturn = Math.max(toReturn, j - i + 1);
+            index[input.charAt(j)] = j + 1;
+        }
+        //Time Complexity : O(n) , j will iterate upto n times ie. input.length
+        return toReturn;
+	}
+
+
+	public static void main(String args[]){
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter String");
+		String input = in.nextLine();
+		int length = getLengthOfLongestSubstring(input);
+		System.out.println(length);
+	}
+}
